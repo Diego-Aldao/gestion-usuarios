28be3d0c3ff0b10e9bbd579764764adc1f8d1860
Diego Aldao <diego_ind@live.com.ar>
2022-08-24 15:43:00 -0300

    añadido un id manual y agregado un unique al email

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -3,9 +3,13 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
+    _id: {
+        type: String,
+        _id: false,
+    } /* para crear un id manual, y el id false para decirle a mongodb que no me cree un id automatico */,
     nombre: { type: String, require: true, minLength: 2, maxLength: 20 },
     apellido: { type: String, require: true, minLength: 4, maxLength: 50 },
-    email: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
     password: { type: String, require: true },
 });
 
