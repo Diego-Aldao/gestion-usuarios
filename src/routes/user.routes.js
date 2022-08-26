290b3e14458a10fdc782b19da0b0a72b804f4828
Diego Aldao <diego_ind@live.com.ar>
2022-08-26 18:42:08 -0300

    agregados los dto y controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,3 +1,10 @@
+import userDeleteController from '#Controllers/user-delete.controller.js';
+import userLoginController from '#Controllers/user-login.controller.js';
+import userPerfilController from '#Controllers/user-profile.controller.js';
+import userRegisterController from '#Controllers/user-register.controller.js';
+import userUpdateController from '#Controllers/user-update-data.controller.js';
+import emailUpdateController from '#Controllers/user-update-email.controller.js';
+import passwordUpdateController from '#Controllers/user-update-password.controller.js';
 import userDeleteDTO from '#Dto/user.delete.dto.js';
 import userJWTDTO from '#Dto/user.jwt.dto.js';
 import userLoginDTO from '#Dto/user.login.dto.js';
@@ -9,18 +16,31 @@ import { Router } from 'express';
 
 const userRouter = Router();
 
-userRouter.post('/registro', userRegistroDTO, (req, res) => {
-    res.send();
-});
+userRouter.post('/registro', userRegistroDTO, userRegisterController);
 
-userRouter.post('/login', userLoginDTO);
+userRouter.post('/login', userLoginDTO, userLoginController);
 
-userRouter.get('/perfil', userJWTDTO);
+userRouter.get('/perfil', userJWTDTO, userPerfilController);
 
-userRouter.patch('/actualizar-datos', userJWTDTO, updateDataDTO);
-userRouter.patch('/actualizar-email', userJWTDTO, updateEmailDTO);
-userRouter.patch('/actualizar-password', userJWTDTO, updatePasswordDTO);
+userRouter.patch(
+    '/actualizar-datos',
+    userJWTDTO,
+    updateDataDTO,
+    userUpdateController
+);
+userRouter.patch(
+    '/actualizar-email',
+    userJWTDTO,
+    updateEmailDTO,
+    emailUpdateController
+);
+userRouter.patch(
+    '/actualizar-password',
+    userJWTDTO,
+    updatePasswordDTO,
+    passwordUpdateController
+);
 
-userRouter.delete('/baja', userDeleteDTO);
+userRouter.delete('/baja', userJWTDTO, userDeleteDTO, userDeleteController);
 
 export default userRouter;
